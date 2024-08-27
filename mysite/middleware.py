d697487843a8468b7e4eb3b9086decd956d49a85
kaan <kaankaraalioglu@yahoo.com>
2024-08-27 13:31:17 +0300

    trying auto-refresh

--- mysite/middleware.py
+++ mysite/middleware.py
@@ -17,6 +17,14 @@ class RedirectToWWWMiddleware:
             new_url = request.build_absolute_uri().replace(host, f'www.{host}')
             logger.debug(f"Redirecting to: {new_url}")
             return HttpResponsePermanentRedirect(new_url)
+
+        if host.startswith('https://') and not settings.DEBUG:
+            new_url = request.build_absolute_uri().replace(host, f'https://www.{host}')
+            logger.debug(f"Redirecting to: {new_url}")
+            return HttpResponsePermanentRedirect(new_url)
+
+
+
         
         response = self.get_response(request)
         return response
