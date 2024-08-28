fb39491cd2898d02dded58177c27f10deee7cbe5
kaan <kaankaraalioglu@yahoo.com>
2024-08-28 13:42:52 +0300

    trying new scheme

--- mysite/middleware.py
+++ mysite/middleware.py
@@ -21,27 +21,21 @@ class RedirectToWWWMiddleware:
             #return HttpResponsePermanentRedirect(new_url)
 
 
-        ## Ensure https with www
-        #if scheme == 'https' and not host.startswith('www.') and not settings.DEBUG:
-            #new_url = request.build_absolute_uri().replace(host, f'www.{host}')
-            #logger.debug(f"Redirecting to: {new_url}")
-            #return HttpResponsePermanentRedirect(new_url) 
+        # Ensure https with www
+        if scheme == 'https' and not host.startswith('www.') and not settings.DEBUG:
+            new_url = request.build_absolute_uri().replace(host, f'www.{host}')
+            logger.debug(f"Redirecting to: {new_url}")
+            return HttpResponsePermanentRedirect(new_url) 
 
-        # Redirect any non-www request to www, maintaining the scheme
-        #if not host.startswith('www.') and not settings.DEBUG:
-            #new_host = f'www.{host}'
-            #new_url = request.build_absolute_uri().replace(host, new_host)
-            #logger.debug(f"Redirecting to: {new_url}")
-            #return HttpResponsePermanentRedirect(new_url)   
-
-
-        if host == 'https://hightech-metrology.com':
-            new_host = 'www.hightech-metrology.com'
+         #Redirect any non-www request to www, maintaining the scheme
+        if not host.startswith('www.') and not settings.DEBUG:
+            new_host = f'www.{host}'
             new_url = request.build_absolute_uri().replace(host, new_host)
             logger.debug(f"Redirecting to: {new_url}")
-            return HttpResponsePermanentRedirect(new_url)     
+            return HttpResponsePermanentRedirect(new_url)   
 
 
+        
         
         response = self.get_response(request)
         return response
