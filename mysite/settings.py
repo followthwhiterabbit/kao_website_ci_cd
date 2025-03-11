36639154e627675fe4e5db0c13d72e8d774b0f53
kaan <kaankaraalioglu@yahoo.com>
2025-03-11 20:25:22 +0300

    gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -190,16 +190,6 @@ STATIC_ROOT = '/home/highpkui/public_html/static' # to run in server 2
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-
-#EMAIL_HOST =  'smtp.gmail.com'
-#EMAIL_USE_TLS = True
-#EMAIL_PORT =  587
-#EMAIL_HOST_USER =  'karaaliogluseyit@gmail.com'
-#EMAIL_HOST_PASSWORD =  'dcxxnbjtzbosptbx'
-
-
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 
 
